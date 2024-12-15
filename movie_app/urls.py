85194a9fc79740587753770ef53916b913af1a50
alishka-m <alimrahimov508@gmail.com>
2024-12-15 21:51:28 +0600

    hw4

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import DirectorViewSet, MovieViewSet, ReviewViewSet
 
 router = DefaultRouter()
-router.register(r"directors", DirectorViewSet)
-router.register(r"movies", MovieViewSet)
-router.register(r"reviews", ReviewViewSet)
+router.register(r"directors", DirectorViewSet, basename="director")
+router.register(r"movies", MovieViewSet, basename="movie")
+router.register(r"reviews", ReviewViewSet, basename="review")
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,34 +1,18 @@
-# movie_app/views.py
-
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from .models import Director, Movie, Review
 from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 
-class DirectorViewSet(viewsets.ModelViewSet):
+class DirectorViewSet(ModelViewSet):
     queryset = Director.objects.all()
     serializer_class = DirectorSerializer
 
-    @action(detail=False, methods=["get"])
-    def directors_with_movies(self, request):
-        directors = Director.objects.annotate(movies_count=models.Count("movies"))
-        serializer = self.get_serializer(directors, many=True)
-        return Response(serializer.data)
 
-
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    @action(detail=False, methods=["get"])
-    def movies_with_reviews(self, request):
-        movies = Movie.objects.all()
-        serializer = self.get_serializer(movies, many=True)
-        return Response(serializer.data)
 
-
-class ReviewViewSet(viewsets.ModelViewSet):
+class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
