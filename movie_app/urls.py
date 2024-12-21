06627e6726baa62d6d0dde2c2fc985b2c5151a97
alishka-m <alimrahimov508@gmail.com>
2024-12-21 10:38:16 +0600

    hw4

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import DirectorViewSet, MovieViewSet, ReviewViewSet
 
 router = DefaultRouter()
-router.register(r"directors", DirectorViewSet, basename="director")
-router.register(r"movies", MovieViewSet, basename="movie")
-router.register(r"reviews", ReviewViewSet, basename="review")
+router.register("directors", DirectorViewSet)
+router.register("movies", MovieViewSet)
+router.register("reviews", ReviewViewSet)
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
